88329d57ca133b50c63140cf02867307fa5dc8d4
LatikDesu <evga.abrosimov@gmail.com>
2023-01-18 17:23:43 +0300

    gameTTT: create board

---
+++ main.py
@@ -0,0 +1,15 @@
+def welcome():
+    print("-------------------")
+    print("       Игра        ")
+    print(' "Крестики-нолики" ')
+    print("-------------------")
+
+board = list(range(1,10))
+def draw_board(board):
+    print ("-" * 13)
+    for i in range(3):
+        print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+        print ("-" * 13)
+
+welcome()
+draw_board(board)
